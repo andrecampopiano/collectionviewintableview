700d5db20afd2abd8718f6270d3cff2494bc8b75
andrecampopiano <andre.campopiano@icloud.com>
2017-01-18 13:29:12 -0200

    Criação Cell de comentários dos usuários

---
+++ ALCCollectionInTableView/ALCCollectionInTableView/Controllers/ViewController/Cell/CellComments/CellComments.h
@@ -0,0 +1,13 @@
+//
+//  CellComments.h
+//  ALCCollectionInTableView
+//
+//  Created by André Campopiano on 18/01/17.
+//  Copyright © 2017 André Campopiano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CellComments : UITableViewCell
+
+@end
---
+++ ALCCollectionInTableView/ALCCollectionInTableView/Controllers/ViewController/Cell/CellComments/CellComments.m
@@ -0,0 +1,24 @@
+//
+//  CellComments.m
+//  ALCCollectionInTableView
+//
+//  Created by André Campopiano on 18/01/17.
+//  Copyright © 2017 André Campopiano. All rights reserved.
+//
+
+#import "CellComments.h"
+
+@implementation CellComments
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
